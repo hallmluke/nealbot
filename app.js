da5e609400a2942ada0c7ff82c7d5438042d915c
Luke Hall <hallmluke@gmail.com>
2018-11-20 19:56:55 -0500

    More setup

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.listen(port, function() {
     // express route defined above.
     .then(() => spark.webhookAdd({
       name: 'my webhook',
-      targetUrl: 'https://example.com/webhook',
+      targetUrl: 'https://nealbot.herokuapp.com/',
       resource: 'all',
       event: 'all',
     }));
