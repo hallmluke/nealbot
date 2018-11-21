8c973345470cec5c7cfdaf018d8a5a1c0022bd37
pearcetj <Pearce_Thomas@bah.com>
2018-11-21 13:53:05 -0500

    banks and drunk

--- reply.js
+++ reply.js
@@ -7,6 +7,21 @@ var matchWordRegex = function(word, text){
     return regexp.test(text);
 }
 
+var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+function intToString (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+    return str;
+}
+
 var getBodyPart = function() {
     int = getRandomInt(6);
     part = null;
@@ -105,7 +120,25 @@ module.exports = function createReply(msg) {
            text = "Not yet. @ me if you fine fellows need anything. I'm just going to be knocking out stories.";
            createMessageObject(msg.roomId, text);
        }
+       if (matchWordRegex("document", msg.text)) {
+           text = "Ive got it in a word doc, Ill move it over once Im done"
+           createMessageObject(msg.roomId, text);
+       }
     }
+    if(matchWordRegex("how much did you", msg.text)){
+        int = 5 - Math.floor(Math.log(getRandomInt(150)+1));
+        wordInt = intToString(int);
+
+        drinks = "boi I had " + wordInt + " bevs last night";
+        if(Math.random() > .7) {
+            drinks = drinks + ". its lit fam"
+        }
+        if(int >= 6) {
+            drinks = drinks + "\nyeah I don't feel so good"
+        }
+        createMessageObject(msg.roomId, drinks);
+    }
+
 
     if(matchWordRegex("saints", msg.text)){
         createMessageObject(msg.roomId, "fuck the saints");
