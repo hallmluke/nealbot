5c1d860c59d3c01752a314f8c97c84693c85ca95
Luke Hall <hallmluke@gmail.com>
2018-11-20 20:07:22 -0500

    Updated webhook endpoint

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.listen(port, function() {
     // express route defined above.
     .then(() => spark.webhookAdd({
       name: 'my webhook',
-      targetUrl: 'https://nealbot.herokuapp.com/',
+      targetUrl: 'https://nealbot.herokuapp.com/webhook',
       resource: 'all',
       event: 'all',
     }));
