557c75fa80c821418064335bd6e153df90195067
pearcetj <Pearce_Thomas@bah.com>
2018-11-26 15:30:49 -0500

    big sweeping weather changes

--- reply.js
+++ reply.js
@@ -173,7 +173,42 @@ var createReply = async function(msg) {
         if(matchWordRegex("weather", msg.text)) {
             console.log("weathering the storm");
             var body = weather();
-            createMessageObject(msg.roomId, "fucking hell this is not worth the effort");
+            weather = JSON.parse(body);
+            resp = null;
+            if (weather.main.temp < 45) {
+                resp = "Its cold as fuck boi\n"
+            }
+            else if(weather.main.temp < 70) {
+                resp = "What an average day\n"
+            }
+            else {
+                resp = "Hot as fuck outside rn\n"
+            }
+            for(i in weather.weather) {
+                if (weather.weather[i].id >= 200 && weather.weather[i].id < 299) {
+                    resp += "These fucking thunderstorms need to go\n"
+                }
+                else if (weather.weather[i].id >= 300 && weather.weather[i] < 399) {
+                    resp += "Its kinda raining outside.  Just enough to be annoying\n"
+                }
+                else if (weather.weather[i].id >= 500 && weather.weather[i] < 599) {
+                    resp += "Its pouring outside rn.  Guess I won't be getting lunch\n"
+                }
+                else if (weather.weather[i].id >= 600 && weather.weather[i].id < 699) {
+                    resp += "Yeah there's snow outside.  Can't drive through this, see you fine folks tomorrow\n"
+                }
+                else if (weather.weather[i].id == 800 ) {
+                    resp += "Would you look at that it's actually a nice day outside\n"
+                }
+                else if (weather.weather[i].id < 800 ) {
+                    resp += "Cloudy outside.  Its cool, I don't like the sun anyways\n"
+                }
+                else {
+                    resp += "There's some crazy shit going on outside.  Fuck this I'm outta here\n"
+                }
+            }
+
+            createMessageObject(msg.roomId, resp);
         }
     
         if(matchWordRegex("be in today", msg.text) || matchWordRegex("in the office", msg.text)) {
